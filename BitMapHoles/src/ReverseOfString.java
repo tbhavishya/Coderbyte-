949197477735ddfbabfdc6b05bc34042fc72c6e1
bhavi <bhavi@LAPTOP-S42NO0PN.lan>
2023-10-30 12:01:35 -0500

    Reverse of a String

---
+++ BitMapHoles/src/ReverseOfString.java
@@ -0,0 +1,28 @@
+
+public class ReverseOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+		   
+		        String s = "hello, world!";
+		        System.out.println(reverseString(s));
+		    }
+
+		    private static String reverseString(String s){
+
+		        String reverse = "";
+
+		        for(int i = s.length() - 1; i >= 0;i--){
+		            reverse += s.charAt(i);
+		        }
+
+		        return reverse;
+		    }
+
+		
+	}
+
+
